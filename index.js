203c5934ad84422e0e4e34df486c58f8eeb14931
Ray Olivier <ray.olivier@projekt202.com>
2020-09-30 10:53:03 -0500

    man it would be easier if i knew the tests wanted single quotes instead of double quotes lmao

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ import bodyParser from 'body-parser';
 const app = express();
 const PORT = 4000;
 
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to database!'));
+
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.get('/', (req, res) =>
-    res.send(`Store server running on port ${PORT}`)
-);
+app.get('/', (req, res) => res.send(`Store server running on port ${PORT}`));
 
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
