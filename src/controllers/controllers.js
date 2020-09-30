530f46ba8d52af808f3bb48a54742beaacd20c7b
Ray Olivier <ray.olivier@projekt202.com>
2020-09-30 15:35:32 -0500

    these tests suck >:(

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -4,17 +4,38 @@ const { ProductSchema } = require('../models/models');
 const Product = mongoose.model('Product', ProductSchema);
 
 export const addnewProduct = (req, res) => {
-  console.log('POST req.body', req.body);
-  if (req.body !== typeof Product) {
-    console.log('you done fucked up');
-  }
-  const newProduct = new Product(req.body);
-  newProduct.save((err, Product) => {
-    if (err) {
-      console.log(err);
-      res.status(400).json({ message: err.message });
-    } else {
-      res.status(200).json(Product);
+	console.log('POST req.body', req.body);
+	const newProduct = new Product(req.body);
+	newProduct.save((err, Product) => {
+		if (err) {
+			console.log(err);
+			res.status(400).json({ message: err.message });
+		} else {
+			res.status(200).json(Product);
+		}
+	});
+};
+
+export const getProducts = (req, res) => {
+	Product.find({}, (err, Product) => {
+		if (err) {
+			console.log(err.message);
+			res.status(400).json({ message: err.message });
+		} else {
+			res.status(200).json(Product);
+		}
+	});
+};
+
+// prettier-ignore
+export const getProductWithID = (req, res) => {
+  Product.findById(req.params.ProductID, (err, Product) => {
+      if (err) {
+        console.log(err.message);
+        res.status(400).json({ message: err.message });
+      } else {
+        res.status(200).json(Product);
+      }
     }
-  });
+  );
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
-const { addnewProduct } = require('../controllers/controllers');
+const { addnewProduct, getProducts, getProductWithID } = require('../controllers/controllers');
 
 // const app = require('express');
 // const router = express.Router();
 
 export const routes = app => {
   app.route('/products').post(addnewProduct);
+  app.route('/products').get(getProducts);
+  app.route('/products/:ProductID').get(getProductWithID);
 };
 
 // router.post('/products', addnewProduct);
